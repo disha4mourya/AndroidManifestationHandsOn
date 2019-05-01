9fe21371880959c42353abe35774909c8eb05da0
disha4mourya <disha4mourya@gmail.com>
2019-05-01 10:32:12 +0530

    fetch all tasks and show on main activity

--- app/src/main/java/com/example/androidmanifestation/MainActivity.java
+++ app/src/main/java/com/example/androidmanifestation/MainActivity.java
@@ -2,26 +2,55 @@ package com.example.androidmanifestation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.telecom.Call;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.androidmanifestation.database.AppDatabase;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-
-public class MainActivity extends AppCompatActivity {
+//implement task click listener
+public class MainActivity extends AppCompatActivity implements TaskAdapter.TaskClickListener {
 
     private RecyclerView rvTasks;
     private FloatingActionButton fabAddTask;
+
+    //Create AppDatabase member variable for the Database
+    private AppDatabase appDatabase;
+    //create member variable for adapter
+    private TaskAdapter taskAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize member variable for the data base
+        appDatabase=AppDatabase.getInstance(this);
+
         intialiseViews();
         setUpAddTaskListener();
+        setAdapterOnRecyclerView();
+    }
+
+    private void setAdapterOnRecyclerView() {
+        //Initialize member variable for adapter
+        taskAdapter=new TaskAdapter(this,this);
+        rvTasks.setLayoutManager(new GridLayoutManager(this,2));
+        //set adapter on recycler view
+        rvTasks.setAdapter(taskAdapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        taskAdapter.setTasks(appDatabase.taskDao().loadAllTasks());
+        //Call the adapter's setTasks method using the result
     }
 
     private void setUpAddTaskListener() {
@@ -37,5 +66,10 @@ public class MainActivity extends AppCompatActivity {
     private void intialiseViews() {
         rvTasks = findViewById(R.id.rvTasks);
         fabAddTask = findViewById(R.id.fabAddTask);
+    }
+
+    @Override
+    public void onTaskClickListener(int taskId) {
+
     }
 }
