443c6241ab1b2690f04debbc6983f72e3c003ee2
disha4mourya <disha4mourya@gmail.com>
2019-05-02 06:04:40 +0530

    instance and menu icon change

--- app/src/main/java/com/example/androidmanifestation/server_calls/retrofit_call/RetrofitInstance.java
+++ app/src/main/java/com/example/androidmanifestation/server_calls/retrofit_call/RetrofitInstance.java
@@ -11,7 +11,7 @@ public class RetrofitInstance {
 
     public static Retrofit getRetrofitInstance(){
         if (retrofit==null){
-            retrofit=new retrofit2.Retrofit.Builder()
+            retrofit=new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
